60a4025139d0a9ca1eff6a377c865bbcbb0ae657
Iago dos Santos <iagosantoscamara@gmail.com>
2025-09-23 21:01:00 -0300

    tkinter centralizado no exercicio05

---
+++ Exercicio - Aula 07/exercicio05.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+root = tk.Tk()
+width = 600
+height = 300
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+x = (screen_width/2) - (width/2)
+y = (screen_height/2) - (height/2)
+
+root.geometry("%dx%d+%d+%d" % (width, height, x, y))
+root.mainloop()
